2842477e3293a549059ff12d98dc73251bb23006
June Chepchirchir <junemaritim@gmail.com>
2026-06-19 01:21:03 +0300

    new update

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ toggleButton.addEventListener('click',function(){
     document.body.classList.toggle('dark-mode');
 
     if(document.body.classList.contains('dark-mode')){
-        toggleButton.textContent = 'Switch to Light Mode';
+        toggleButton.textContent =  'Light Mode';
     } else {
-        toggleButton.textContent = 'Switch to Dark Mode';
+        toggleButton.textContent = 'Dark Mode';
     }
 });
